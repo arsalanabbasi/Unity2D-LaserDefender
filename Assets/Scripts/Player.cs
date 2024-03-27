3d95b953b76710926196b9613c4f4d6657d8d584
Arsalan Abbasi <abbasiarsalan25@gmail.com>
2024-03-27 09:12:05 +0500

    Setting up enemies wave & path logic

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,15 +7,36 @@ public class Player : MonoBehaviour
 {
     Vector2 inputValue;
     [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float paddingLeft;
+    [SerializeField] float paddingRight;
+    [SerializeField] float paddingTop;
+    [SerializeField] float paddingBottom;
 
-    void Update()
-    {
+    Vector2 minBounds;
+    Vector2 maxBounds;
+
+    void Start(){
+        
+        InitBounds();
+        }
+
+    void Update(){
+        
         Movement();
-    }
+        }
+
+    void InitBounds(){
+        Camera mainCamera = Camera.main;
+        minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0,0));
+        maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1,1));
+        }
 
     void Movement(){
-        Vector3 delta = inputValue * moveSpeed * Time.deltaTime;
-        transform.position += delta;
+        Vector2 delta = inputValue * moveSpeed * Time.deltaTime;
+        Vector2 newPos = new Vector2();
+        newPos.x = Mathf.Clamp(transform.position.x + delta.x, minBounds.x + paddingLeft, maxBounds.x - paddingRight);
+        newPos.y = Mathf.Clamp(transform.position.y + delta.y, minBounds.y + paddingBottom, maxBounds.y - paddingTop);
+        transform.position = newPos;
         }
 
     void OnMove(InputValue value){
