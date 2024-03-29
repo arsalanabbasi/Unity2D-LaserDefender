a8491c4703849b78006cd2d1bf3531b11489a6d7
Arsalan Abbasi <abbasiarsalan25@gmail.com>
2024-03-30 03:10:00 +0500

    Instantiating Enemies

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] WaveConfigSO currentWave;
+    void Start()
+    {
+        SpawnEnemies();
+    }
+
+    public WaveConfigSO GetCurrentWave(){
+        return currentWave;
+        }
+
+    void SpawnEnemies(){
+        
+        for(int i=0; i< currentWave.GetEnemyCount(); i++){
+            
+            Instantiate(currentWave.GetEnenmyPrefab(i),
+                        currentWave.GetStartingWayPoint().position,
+                        Quaternion.identity, transform);
+            }
+        
+
+        }
+}
--- Assets/Scripts/WaveConfigSO.cs
+++ Assets/Scripts/WaveConfigSO.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 [CreateAssetMenu(menuName = "Wave Config", fileName = "Wave Config")]
 public class WaveConfigSO : ScriptableObject
-{
+{   
+    [SerializeField] List<GameObject> enemyPrefabs;
     [SerializeField] Transform pathPrefab;
     [SerializeField] float moveSpeed = 11f;
 
@@ -24,5 +25,13 @@ public class WaveConfigSO : ScriptableObject
 
     public float GetMoveSpeed(){
         return moveSpeed;
+        }
+
+    public int GetEnemyCount(){
+        return enemyPrefabs.Count;
+        }   
+
+    public GameObject GetEnenmyPrefab(int index){
+        return enemyPrefabs[index];
         }
 }
